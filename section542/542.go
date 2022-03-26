023c61e441f24477d4004bcdd4d2f8018664270b
unknown <andy@126.com>
2022-03-27 00:17:38 +0800

    多层bfs

--- section542/542.go
+++ section542/542.go
@@ -1,61 +1,145 @@
 package main
 
+import "math"
 
 var (
 	dx = []int{0,1,0,-1}
 	dy = []int{1,0,-1,0}
 )
 
+type Point struct {
+	x, y int
+}
+
+func updateMatrix0(mat [][]int) [][]int {
+	var (
+		m = len(mat)
+		n = len(mat[0])
+		dist = make([][]int, m)
+		seen = make([][]bool, m)
+		st []Point
+	)
+
+	for i := 0; i < m; i++ {
+		dist[i] = make([]int, n)
+		seen[i] = make([]bool, n)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if mat[i][j] == 0 {
+				dist[i][j] = 0
+				seen[i][j] = true
+				st = append(st, Point{x: i, y: j})
+			}
+		}
+	}
+
+	for len(st) > 0 {
+		p := st[0]
+		st = st[1:]
+		for i := 0; i < 4; i++ {
+			px, py := p.x + dx[i], p.y + dy[i]
+			if px >= 0 && px < m && py >= 0 && py < n && !seen[px][py] {
+				seen[px][py] = true
+				dist[px][py] = dist[p.x][p.y] + 1
+				st = append(st, Point{x: px, y: py})
+			}
+		}
+	}
+
+	return dist
+}
+
+
+
+
+
+
+
+
+
+
 func updateMatrix(mat [][]int) [][]int {
 	var (
 		m = len(mat)
 		n = len(mat[0])
-		t = make(map[int]int)
+		dist = make([][]int, m)
 	)
+
 	for i := 0; i < m; i++ {
+		dist[i] = make([]int, n)
 		for j := 0; j < n; j++ {
 			if mat[i][j] == 0 {
-				continue
-			}
-			t[i * m + j] = bfs(i, j, m, n, mat)
-		}
-	}
-	return mat
-}
-
-func bfs(i, j, m, n int, mat [][]int) int {
-	var (
-		queue []int
-		list []int
-		deep = 0
-	)
-	queue = append(queue, i * m + j)
-	for len(queue) > 0 {
-		x, y := queue[0] / m, queue[0] % m
-		queue = queue[1:]
-		for k := 0; k < 4; k++ {
-			kx, ky := dx[k] + x, dy[k] + y
-			if kx >= 0 && kx < m && ky >= 0 && ky < n {
-				if mat[kx][ky] == 0 {
-					return deep
-				} else {
-					list = append(list, kx * m + ky)
-				}
-			}
-		}
-		if len(queue) == 0 {
-			if len(list) == 0 {
-				deep = 0
+				dist[i][j] = 0
 			} else {
-				deep += 1
-				queue = append(queue, list...)
-				list = list[0:0]
+				dist[i][j] = math.MaxInt / 2
 			}
 		}
 	}
 
-	return deep
+	// 右上
+	for i := 0; i < m; i++ {
+		for j := n - 1; j  >= 0; j-- {
+			if i - 1 >= 0 {
+				dist[i][j] = minInt(dist[i][j], dist[i - 1][j] + 1)
+			}
+			if j + 1 < n  {
+				dist[i][j] = minInt(dist[i][j], dist[i][j + 1] + 1)
+			}
+		}
+	}
+
+	// 右下
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i - 1 >= 0 {
+				dist[i][j] = minInt(dist[i][j], dist[i - 1][j] + 1)
+			}
+			if j - 1 >= n  {
+				dist[i][j] = minInt(dist[i][j], dist[i - 1][j] + 1)
+			}
+		}
+	}
+
+	// 左上
+	for i := m - 1; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			if i + 1 < m {
+				dist[i][j] = minInt(dist[i][j], dist[i + 1][j] + 1)
+			}
+			if j + 1 < n  {
+				dist[i][j] = minInt(dist[i][j], dist[i][j + 1] + 1)
+			}
+		}
+	}
+
+	// 左下
+	for i := m - 1; i >= 0; i-- {
+		for j := 0; j < n; j++ {
+			if i + 1 < m {
+				dist[i][j] = minInt(dist[i][j], dist[i + 1][j] + 1)
+			}
+			if j - 1 >= 0  {
+				dist[i][j] = minInt(dist[i][j], dist[i][j - 1] + 1)
+			}
+		}
+	}
+
+	return dist
 }
+
+func minInt(x, y int) int {
+	if x >= y {
+		return y
+	}
+	return x
+}
+
+
+
+
+
 
 func main() {
 	mat := [][]int{
--- section994/994.go
+++ section994/994.go
@@ -1,15 +1,68 @@
 package section994
 
+var (
+	dx = []int{0,1,0,-1}
+	dy = []int{1,0,-1,0}
+)
+
+type Point struct {
+	x, y int
+}
 
 func orangesRotting(grid [][]int) int {
-	m, n, to, fo := len(grid), len(grid[0]), 0, 0
-	for i := 0; i < m * n; i++ {
-		r, c := i / n, i % n
-		if grid[r][c] == 2 { // 腐烂的橘子
-			fo = 2
+	var (
+		m = len(grid)
+		n = len(grid[0])
+		seen = make([][]bool, m)
+		st []Point
+		t = -1
+		s = 0
+	)
+
+	for i := 0; i < m; i++ {
+		seen[i] = make([]bool, n)
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 0 { // 空格
+				grid[i][j] = -1
+			} else if grid[i][j] == 2 { // 腐烂的橘子
+				grid[i][j] = 0
+				seen[i][j] = true
+				st = append(st, Point{x: i, y: j})
+				t = 0
+			} else {
+				s += 1
+			}
 		}
 	}
-	return to
+
+	if t == -1 {
+		if s == 0 {
+			return 0
+		}
+		return -1
+	}
+
+	for len(st) > 0 {
+		p := st[0]
+		st = st[1:]
+		for i := 0; i < 4; i++ {
+			px, py := p.x + dx[i], p.y + dy[i]
+			if px >= 0 && px < m && py >= 0 && py < n && !seen[px][py] && grid[px][py] == 1 {
+				grid[px][py] = grid[p.x][p.y] + 1
+				seen[px][py] = true
+				st = append(st, Point{x: px, y: py})
+				s--
+				if grid[px][py] > t {
+					t = grid[px][py]
+				}
+			}
+		}
+	}
+
+	if s == 0 {
+		return t
+	}
+	return -1
 }
 
 
